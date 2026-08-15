f103953b8271dd004d4aaf469684fc6745457d7c
Bhargav Kantheti <k@bk.bio>
2026-08-15 07:37:42 -0500

    ADD tailwind

--- ext/wxt.config.ts
+++ ext/wxt.config.ts
@@ -1,8 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "wxt";
 
 export default defineConfig({
   manifestVersion: 3,
   modules: ["@wxt-dev/module-react"],
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
   webExt: {
     disabled: true,
   },
